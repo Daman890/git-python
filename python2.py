4ca9862ee28aaf8547caa9dabf7b123663d2a098
Daman <daman@DESKTOP-R8C6RIN.localdomain>
2026-09-09 15:16:56 +0000

    python2.py

---
+++ python2.py
@@ -0,0 +1,11 @@
+# A 'for' loop that counts from 0 to 4
+for i in range(5):
+    print(f"Count: {i}")
+
+# A 'while' loop that runs until a condition is met
+countdown = 3
+while countdown > 0:
+    print(countdown)
+    countdown -= 1
+print("Blast off!")
+
